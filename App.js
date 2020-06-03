503914ac1712fd8af7ce74cc5abd82479ecac9c7
Stephen Kridel <stephen.kridel@gmail.com>
2020-06-02 20:17:44 -0400

    Initial commit. Bare HomeScreen and Navigator setup and connected.

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './src/screens/HomeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
   },
 });
+
+export default createAppContainer(AppNavigator);
