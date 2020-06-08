42eb6176b0c87a73b61ac201b6fb6b8df615e70b
Stephen Kridel <stephen.kridel@gmail.com>
2020-06-08 17:21:43 -0400

    Added in state to track whether audio is playing and to change the text on the Play/Pause button based on whether audio is playing or not.

--- src/screens/SessionScreen.js
+++ src/screens/SessionScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import PlayMusic from '../modules/PreMixedAudio';
 
@@ -6,14 +6,34 @@ const SessionScreen = props => {
 
     const file = props.navigation.getParam('file');
 
+    const [playing, setPlaying] = useState(false);
+
+    const [text, setText] = useState('Play');
+
     return (
         <View style={styles.Hero}>
             <Text style={styles.HeroText}>Session Screen</Text>
             <View style={styles.Controls}>
-                <TouchableOpacity onPress={() => PlayMusic(file)} style={styles.Module}>
-                    <Text>Play Session</Text>
+                <TouchableOpacity 
+                    onPress={() => {
+                        // PlayMusic(file);
+                        if (playing) {
+                            setPlaying(false);
+                            setText('Play');
+                        } else {
+                            setPlaying(true);
+                            setText('Pause');
+                        };
+                    }} 
+                    style={styles.Module}>
+                    <Text>{text} Session</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => PlayMusic(file)} style={styles.Module}>
+                <TouchableOpacity 
+                    onPress={() => {
+                        // PlayMusic(file);
+                        setPlaying(false);
+                    }}
+                    style={styles.Module}>
                     <Text>Stop Session</Text>
                 </TouchableOpacity>
             </View>
