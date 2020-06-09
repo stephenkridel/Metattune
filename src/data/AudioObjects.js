b7bae062d16ecdd3d7b214aab8d3ac51e2615028
Stephen Kridel <stephen.kridel@gmail.com>
2020-06-09 13:09:25 -0400

    Added a class called PremixedAudioElement. Changed the explicit object declarations to calls of the new class.

--- src/data/AudioObjects.js
+++ src/data/AudioObjects.js
@@ -1,15 +1,21 @@
 export default function AudioObjects() {
-    const nature = {
-        title: 'Nature',
-        file: require("../assets/nature.mp3"),
-        isPremixed: true
-    }
     
-    const urban = {
-        title: 'Urban',
-        file: require("../assets/nature.mp3"),
-        isPremixed: true
+    class PremixedAudioElement {
+        constructor(title, file) {
+            this.title = title;
+            this.file = file;
+        }
     }
+
+    const nature = new PremixedAudioElement(
+        'Nature',
+        require("../assets/nature.mp3"),
+    )
+    
+    const urban = new PremixedAudioElement(
+        'Urban',
+        require("../assets/nature.mp3"),
+    )
 
     return { nature, urban }
 };
