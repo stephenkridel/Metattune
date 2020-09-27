aeb89553043a215dce41a597272de3fbbaaf4f23
Stephen Kridel <stephen.kridel@gmail.com>
2020-09-27 16:30:33 -0400

    No changes. Just git thinking there are changes.

--- src/helpers/SoundBitesAndTimers.js
+++ src/helpers/SoundBitesAndTimers.js
@@ -2,60 +2,60 @@ import Timer from '../helpers/Timer.js';
 import { Audio } from 'expo-av';
 
 const randomizeSoundBites = array => {
-    return new Promise((resolve, reject) => {
-        // doubling the size of the array
-        array = array.concat(array);
+	return new Promise((resolve, reject) => {
+		// doubling the size of the array
+		array = array.concat(array);
 
-        // Fisher-Yates shuffle
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
+		// Fisher-Yates shuffle
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
 
-        // null is loosely equivalent to undefined (i.e. null == undefined) so
-        // we get away with only checking for null values with loose equivalence
-        // not great error checking but it can be adapted later
-        if (array == null) {
-            reject('error in randomizeSoundBites function');
-        } else {
-            resolve(array);
-        }
-    });
+		// null is loosely equivalent to undefined (i.e. null == undefined) so
+		// we get away with only checking for null values with loose equivalence
+		// not great error checking but it can be adapted later
+		if (array == null) {
+			reject('error in randomizeSoundBites function');
+		} else {
+			resolve(array);
+		}
+	});
 };
 
 const loadSoundBiteAudio = array => {
-    return new Promise((resolve, reject) => {
-        try {
-            array = array.map(element => {
-                const soundObject = new Audio.Sound();
-                soundObject.loadAsync(element);
-                return soundObject;
-            });
-        } catch (error) {
-            throw 'error loading the audio prompts (expo async error)';
-        }
-        if (array == null) {
-            reject('error in loadSoundBiteAudio function');
-        } else {
-            resolve(array);
-        }
-    });
+	return new Promise((resolve, reject) => {
+		try {
+			array = array.map(element => {
+				const soundObject = new Audio.Sound();
+				soundObject.loadAsync(element);
+				return soundObject;
+			});
+		} catch (error) {
+			throw 'error loading the audio prompts (expo async error)';
+		}
+		if (array == null) {
+			reject('error in loadSoundBiteAudio function');
+		} else {
+			resolve(array);
+		}
+	});
 };
 
 const setupTimers = soundArray => {
-    return new Promise((resolve, reject) => {
-        var timerArray = [];
-        for (let i = 0; i < soundArray.length; i++) {
-            timerArray[i] = new Timer(function () {
-                soundArray[i].playAsync();
-            }, i * 53000);
-        }
-        if (timerArray == null || soundArray == null) {
-            reject('error in setupTimers function');
-        } else {
-            resolve([timerArray, soundArray]);
-        }
-    });
+	return new Promise((resolve, reject) => {
+		var timerArray = [];
+		for (let i = 0; i < soundArray.length; i++) {
+			timerArray[i] = new Timer(function () {
+				soundArray[i].playAsync();
+			}, i * 53000);
+		}
+		if (timerArray == null || soundArray == null) {
+			reject('error in setupTimers function');
+		} else {
+			resolve([timerArray, soundArray]);
+		}
+	});
 };
 
 export { randomizeSoundBites, loadSoundBiteAudio, setupTimers };
--- src/helpers/timer.js
+++ src/helpers/timer.js
@@ -1,34 +1,34 @@
 export default class Timer {
-    constructor(callback, delay) {
-        this.delay = delay;
-        this.callback = callback;
-        this.instance = null;
-        this.remaining = delay;
-        this.startTime = null;
+	constructor(callback, delay) {
+		this.delay = delay;
+		this.callback = callback;
+		this.instance = null;
+		this.remaining = delay;
+		this.startTime = null;
 
-        this.start = () => {
-            this.startTime = Date.now();
-            this.remaining -= Date.now() - this.startTime;
-            clearTimeout(this.instance);
-            if (this.remaining > 0) {
-                clearTimeout(this.instance);
-                this.instance = setTimeout(this.callback, this.remaining);
-            }
-        };
+		this.start = () => {
+			this.startTime = Date.now();
+			this.remaining -= Date.now() - this.startTime;
+			clearTimeout(this.instance);
+			if (this.remaining > 0) {
+				clearTimeout(this.instance);
+				this.instance = setTimeout(this.callback, this.remaining);
+			}
+		};
 
-        this.pause = () => {
-            this.remaining -= Date.now() - this.startTime;
-            clearTimeout(this.instance);
-        };
+		this.pause = () => {
+			this.remaining -= Date.now() - this.startTime;
+			clearTimeout(this.instance);
+		};
 
-        this.stop = () => {
-            this.remaining = this.delay;
-            clearTimeout(this.instance);
-        };
+		this.stop = () => {
+			this.remaining = this.delay;
+			clearTimeout(this.instance);
+		};
 
-        this.destroy = () => {
-            clearTimeout(this.instance);
-            this.instance = null;
-        };
-    }
+		this.destroy = () => {
+			clearTimeout(this.instance);
+			this.instance = null;
+		};
+	}
 }
