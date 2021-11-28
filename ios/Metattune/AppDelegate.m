d0f5367e17194cfb03406f963ffe35a1d99d33a5
Stephen Kridel <stephen.kridel@gmail.com>
2021-11-28 14:09:48 -0500

    Renamed app Metattune using react-native-rename and also changed the git repo name. Did not change the bundle id

--- ios/Metattune/AppDelegate.m
+++ ios/Metattune/AppDelegate.m
@@ -53,7 +53,7 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"Attune"
+                                                   moduleName:@"Metattune"
                                             initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
