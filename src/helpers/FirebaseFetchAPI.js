4f4c88c29a92abdd323bad5b876e39a49c68dcc7
Stephen Kridel <stephen.kridel@gmail.com>
2022-03-11 10:23:56 -0800

    Got all the updated dependencies working on ios. Configured bootsplash to work. Updated macos to Monterrey and xcode to the latest version. Sessions not loading because of _updateStatistics function and the logo isn't showing up.

--- src/helpers/FirebaseFetchAPI.js
+++ src/helpers/FirebaseFetchAPI.js
@@ -4,8 +4,8 @@ import { updateProgressMessage } from '../actions/ProgressActions';
 
 export default class FirebaseFetchAPI {
   static fetchMedia = async mediaFile => {
-    //let filePath = '/assets/sounds/test/' + mediaFile + '_test.mp3';
-    let filePath = '/assets/sounds/production/' + mediaFile + '.mp3';
+    let filePath = '/assets/sounds/test/' + mediaFile + '_test.mp3';
+    //let filePath = '/assets/sounds/production/' + mediaFile + '.mp3';
     let mediaFileName = mediaFile.charAt(0).toUpperCase() + mediaFile.slice(1);
     store.dispatch(
       updateProgressMessage(`Loading Media File: ${mediaFileName}`),
